9b55e7fca074f2a334849252fd859609cf8d8eeb
Mateusz Gorecki <mateusz2602@gmail.com>
2025-03-24 15:05:32 +0000

    update

--- src/chatbox/nlp.py
+++ src/chatbox/nlp.py
@@ -1,20 +1,28 @@
 import spacy
 import pandas as pd
 from spacy.matcher import PhraseMatcher
+from difflib import get_close_matches
 import spellchecker
 from datetime import datetime
+from spacy.lang.en.stop_words import STOP_WORDS
 
 def setup():
     global nlp, spell, matcher, cleaned_stations, stations_df
     nlp = spacy.load("en_core_web_sm")
     spell = spellchecker.SpellChecker()
-
     stations_df = pd.read_csv("src/data/stations.csv")
     second_column = stations_df.iloc[:, 1]
-    cleaned_stations = [name.replace(" Rail Station", "").strip().lower() for name in second_column]
-    cleaned_stations_2 = [word for name in cleaned_stations for word in name.split()]
-    spell.word_frequency.load_words(cleaned_stations_2)
+    cleaned_stations = []
 
+    for name in second_column:
+        name = name.replace(" Rail Station", "").strip()
+        name = name.replace("-", " ")
+        name = name.replace("(", "").replace(")", "")
+        name = name.lower()
+        cleaned_stations.append(name)
+
+
+    spell.word_frequency.load_words(cleaned_stations)
     matcher = PhraseMatcher(nlp.vocab)
     patterns = [nlp.make_doc(station) for station in cleaned_stations]
     matcher.add("STATION", patterns)
@@ -22,6 +30,7 @@ def setup():
 def correct_spelling(word):
     corrected_word = spell.correction(word)
     return corrected_word if corrected_word else word
+
 
 
 def extract_time(sentence):
@@ -62,19 +71,38 @@ def find_stations(sentence):
         if f"from {station}" in sentence_lower:
             from_station = station
             detected_stations.remove(station)
+            sentence_lower = sentence_lower.replace(f"from {station}", "")
         elif f"to {station}" in sentence_lower:
             to_station = station
             detected_stations.remove(station)
-
+            sentence_lower = sentence_lower.replace(f"to {station}", "")
 
     if (from_station is None) and (len(detected_stations) > 1):
         from_station = detected_stations[0]
+        sentence_lower = sentence_lower.replace(from_station, "")
 
     if (to_station is None) and (len(detected_stations) > 1):
         to_station = detected_stations[0]
+        sentence_lower = sentence_lower.replace(to_station, "")
 
+    if from_station is None or to_station is None:
+        print(find_closest_station(sentence_lower))
 
     return from_station, to_station
+
+def find_closest_station(sentence):
+    sentence_corrected = " ".join(correct_spelling(word) for word in sentence.split())
+    doc = nlp(sentence_corrected)
+
+    # Remove stop words from the sentence
+    filtered_words = [word for word in sentence_corrected.lower().split() if word not in STOP_WORDS]
+
+    # Check for partial matches in cleaned_stations
+    partial_matches = []
+    for word in filtered_words:
+        partial_matches.extend([station for station in cleaned_stations if word in station])
+    partial_matches = list(set(partial_matches))
+    return partial_matches[:4] if partial_matches else None
 
 
 def get_coded_stations(station):
@@ -92,4 +120,3 @@ if __name__ == "__main__":
     while True:
         sentence = input("Enter a sentence: ")
         print(find_stations(sentence))
-        print(extract_time(sentence))
