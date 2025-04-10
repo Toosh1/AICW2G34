dfe685365d2df29b0b7e1504028d0e903fecc6b1
Ace <aceshyjan@gmail.com>
2025-04-10 19:28:52 +0100

    feat closest stations

--- src/chatbot/nlp.py
+++ src/chatbot/nlp.py
@@ -10,6 +10,7 @@ import re
 import spacy
 from spacy.matcher import Matcher
 from spellchecker import SpellChecker
+from rapidfuzz import process
 from knowledge_base import station_codes, get_departure_arrival_patterns, get_prepositions
 
 # Load spaCy's English model
@@ -28,6 +29,10 @@ def setup():
 
 
 def add_stations_to_vocab():
+    '''
+    Add station names to the spaCy vocabulary for spell checking.
+    :return: None
+    '''
     stations_list = station_codes.keys()
     station_words = {token for station in stations_list for token in station.split()}
     spell.word_frequency.load_words(station_words)
@@ -39,8 +44,16 @@ def add_station_entity_ruler():
     :return: None
     '''
     ruler = nlp.add_pipe("entity_ruler", config={"overwrite_ents": True}, name="station_ruler", before="ner")
-    patterns = [{"label": "GPE", "pattern": station} for station in station_codes.keys()]
-    ruler.add_patterns(patterns)
+    
+    stations = list(station_codes.keys())
+    # Remove any overlapping names
+    places = {station.split(" ")[0] for station in stations} - set(stations)
+
+    station_patterns = [{"label": "STATION", "pattern": station} for station in stations]
+    place_patterns = [{"label": "PLACE", "pattern": place} for place in places]
+    
+    ruler.add_patterns(station_patterns)
+    ruler.add_patterns(place_patterns)        
 
 
 def add_matcher_patterns():
@@ -61,7 +74,24 @@ def add_matcher_patterns():
     #     matcher.add(intent, patterns)
 
 
+def correct_spelling(word) -> str:
+    '''
+    Correct the spelling of a word using the spell checker.
+    :param word: The word to correct.
+    :return: The corrected word.
+    '''
+    corrected_word = spell.correction(word)
+    return corrected_word if corrected_word else word
+
+
 def extract_station(type: str, text: str, terms: list) -> str:
+    '''
+    Extract the station name from the text using regex.
+    :param type: The type of station (departure or arrival).
+    :param text: The input text to search for the station name.
+    :param terms: The list of terms to search for.
+    :return: The extracted station name or None if not found.
+    '''
     # Return station name if already found
     if not (type is None):
         return type
@@ -70,6 +100,18 @@ def extract_station(type: str, text: str, terms: list) -> str:
         match = re.search(rf"\b{re.escape(term)}\b", text, re.IGNORECASE)
         if match:
             return re.sub(rf"\b{re.escape(term)}\b", "", text, count=1, flags=re.IGNORECASE).strip()
+
+
+def find_closest_stations(query: str) -> list:
+    '''
+    Suggest the closest matching station names based on the query.
+    :param query: The input query string
+    :return: A list of similar station names
+    '''
+    station_names = list(station_codes.keys())
+    similar_stations = process.extract(query, station_names, limit=3)
+    # Return only the station names
+    return [station[0] for station in similar_stations]
 
 
 def extract_train_info(user_input):
@@ -94,19 +136,28 @@ def extract_train_info(user_input):
     
     for _, start, end in matches:
         span = doc[start:end]
-
+        print(f"Matched span: {span.text}")
         departure = extract_station(departure, span.text.lower(), departure_terms)
         arrival = extract_station(arrival, span.text.lower(), arrival_terms)
         
+    similar_stations = [
+        find_closest_stations(ent.text) for ent in doc.ents if ent.label_ == "PLACE"
+    ]
+    
     print(f"Departure: {departure}")
     print(f"Arrival: {arrival}")
+    print(f"Similar stations: {similar_stations}")
+    
+    for ent in doc.ents:
+        print(f"Entity: {ent.text}, Label: {ent.label_}")
 
 
 def preprocess_text(text):
     '''
     Preprocess the input text by normalizing it to lower case and removing special characters.
     :param text: The input text to preprocess.
-    :return: The preprocessed text.'''
+    :return: The preprocessed text.
+    '''
     
     # Normalize input text to lower case and remove special characters
     cleaned_text = text.lower()
@@ -116,7 +167,7 @@ def preprocess_text(text):
     tokens = cleaned_text.split()
     
     # Loop through tokens and correct spelling
-    corrected_tokens = [spell.correction(token) for token in tokens]
+    corrected_tokens = [correct_spelling(token) for token in tokens]
     
     # Apply lemmatization
     lemmatized_tokens = [token.lemma_ for token in nlp(" ".join(corrected_tokens))]
