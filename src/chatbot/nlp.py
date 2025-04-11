46989d8f6dc14147818dbcf279215b867042b133
Ace <aceshyjan@gmail.com>
2025-04-11 19:26:09 +0100

    refactor into utils

--- src/chatbot/nlp.py
+++ src/chatbot/nlp.py
@@ -12,42 +12,46 @@ To Do
     - Get type of railcard
 '''
 
+# Import necessary libraries
+import sys
+import os
 import re
 import spacy
-import dateparser
 from spacy.matcher import Matcher
-from spellchecker import SpellChecker
 from rapidfuzz import process
+
+# Merge the parent directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Import custom modules
 from database import *
-from datetime import datetime
+from utils.input_handler import add_to_vocabulary, preprocess_text, preprocess_time, parse_time
+
 
 # Load spaCy's English model
 nlp = spacy.load("en_core_web_sm")
-spell = SpellChecker()
 matcher = None
 
 
-def setup():
+def setup() -> None:
     add_stations_to_vocab()
     add_series_entity_ruler()
     add_station_entity_ruler()
     add_matcher_patterns()
-
-    # Load the custom knowledge base
-    spell.word_frequency.load_words(station_codes.keys())
+    add_to_vocabulary(station_codes.keys())
 
 
-def add_stations_to_vocab():
+def add_stations_to_vocab() -> None:
     '''
     Add station names to the spaCy vocabulary for spell checking.
     :return: None
     '''
     stations_list = station_codes.keys()
     station_words = {token for station in stations_list for token in station.split()}
-    spell.word_frequency.load_words(station_words)
+    add_to_vocabulary(station_words)
 
 
-def add_station_entity_ruler():
+def add_station_entity_ruler() -> None:
     '''
     Add a custom entity ruler to the spaCy pipeline for station names.
     :return: None
@@ -65,7 +69,7 @@ def add_station_entity_ruler():
     ruler.add_patterns(place_patterns)        
 
 
-def add_series_entity_ruler():
+def add_series_entity_ruler() -> None:
     '''
     Add a custom entity ruler to the spaCy pipeline for series names.
     :return: None
@@ -79,7 +83,7 @@ def add_series_entity_ruler():
     ruler.add_patterns(month_pattern)
 
 
-def add_matcher_patterns():
+def add_matcher_patterns() -> None:
     '''
     Add custom patterns to the spaCy matcher for train routes.
     This includes departure and arrival patterns.
@@ -91,88 +95,11 @@ def add_matcher_patterns():
     matcher.add("PREPOSITIONS", get_extraction_patterns(), greedy="LONGEST")
 
 
-def preprocess_text(text: str, spell_check: bool =True):
-    '''
-    Preprocess the input text by normalizing it to lower case and removing special characters.
-    :param text: The input text to preprocess.
-    :return: The preprocessed text.
-    '''
-    # Normalize input text to lower case and remove special characters
-    cleaned_text = re.sub(r"[^a-z0-9\s]", "", text)
-    cleaned_text = re.sub(r"\s+", " ", cleaned_text).strip()
-    
-    tokens = cleaned_text.split()
-    
-    # Loop through tokens and correct spelling
-    if spell_check:
-        # Correct spelling of tokens
-        tokens = [correct_spelling(token) for token in tokens]
-    
-    # Apply lemmatization
-    corrected_tokens = [token.lemma_ for token in nlp(" ".join(tokens))]
-        
-    text = " ".join(corrected_tokens).upper()
-    
-    return text
-
-
-def add_leading_zero(match: re.Match) -> str:
-    '''
-    Add leading zero to the hour in the time string.
-    :param match: The regex match object.
-    :return: The time string with leading zero.
-    '''
-    return f'{int(match.group(1)):02}:00{match.group(2)}'
-    
-
-def format_time(time_str: str) -> str:
-    '''
-    Format the time string to a standard format.
-    :param time_str: The input time string.
-    :return: The formatted time string.
-    '''
-    
-    # Regex pattern to find times like '1am', '1pm', etc and convert them to '01:00am', '01:00pm'
-    pattern = r'(?<=\b)(\d{1})(am|pm)\b'
-    time_str = re.sub(pattern, add_leading_zero, time_str)
-    
-    # Regex to remove any space between the time and the 'AM/PM'
-    time_str = re.sub(r'(\d{2}:\d{2})(am|pm)', r'\1 \2', time_str, flags=re.IGNORECASE)
-
-    return time_str
-
-
-def preprocess_time(text: str) -> str:
-    '''
-    Preproces the input text for time, remove extra spaces and correct am/pm format.
-    :param text: The input text to preprocess.
-    :return: The preprocessed text.
-    '''
-    cleaned_text = text.capitalize()
-    
-    cleaned_text = format_time(cleaned_text)
-    
-    return cleaned_text
-
-
 def extract_passengers(text: str) -> list:
     text = preprocess_text(text)
     adults = None
     children = None
     return []
-
-
-def parse_time(journey: dict[str, str]) -> str:
-    parsed_date = datetime.now()
-    
-    for key in journey.keys():
-        for value in journey[key]:
-            settings = {"PREFER_DATES_FROM": "future", "RELATIVE_BASE": parsed_date, "TIMEZONE": "GMT", "DATE_ORDER": "DMY"}
-            result = dateparser.parse(value, settings=settings)
-            if not result:
-                continue
-            parsed_date = result
-    return parsed_date
 
 
 def extract_date_time(text: str) -> str:
@@ -187,8 +114,6 @@ def extract_date_time(text: str) -> str:
     text = preprocess_time(text)
     doc = nlp(text)
     journeys = {"DATE": [], "TIME": []}
-    
-    print(text)
     
     for ent in doc.ents:
         if not ent.label_ in TIME_ENTITIES:
@@ -206,20 +131,7 @@ def extract_date_time(text: str) -> str:
             case "MONTH":
                 journeys["DATE"].append(ent.text)
 
-    time = parse_time(journeys)
-    print(f"Parsed time: {time}")
-    
-    return journeys
-
-
-def correct_spelling(word) -> str:
-    '''
-    Correct the spelling of a word using the spell checker.
-    :param word: The word to correct.
-    :return: The corrected word.
-    '''
-    corrected_word = spell.correction(word)
-    return corrected_word if corrected_word else word
+    return journeys, parse_time(journeys)
 
 
 def extract_station(type: str, text: str, terms: list) -> str:
@@ -282,7 +194,7 @@ def extract_train_info(text: str) -> tuple:
     return departure, arrival, similar_stations    
 
 
-def main():
+def main() -> None:
     '''
     Main function to run the chatbot.
     It initializes the chatbot and handles user input.
@@ -294,8 +206,12 @@ def main():
     while True:
         user_input = input("You: ")
         departure, arrival, similar_stations = extract_train_info(user_input)
-        journeys = extract_date_time(user_input)
-        print(f"Journeys: {journeys}")
+        journeys, date_time = extract_date_time(user_input)
+        print(f"Departure: {departure}")
+        print(f"Arrival: {arrival}")
+        print(f"Similar stations: {similar_stations}")
+        print(f"Journeys: {', '.join(f'{key}: {", ".join(values)}' for key, values in journeys.items() if values)}")
+        print(f"Date and Time: {date_time}")
         print("--" * 30)
 
 
---
+++ src/utils/input_handler.py
@@ -0,0 +1,98 @@
+import re
+from spellchecker import SpellChecker
+from datetime import datetime
+import dateparser
+
+spell = SpellChecker()
+
+
+def add_to_vocabulary(words: list) -> None:
+    spell.word_frequency.load_words(words)
+
+
+def correct_spelling(word: str) -> str:
+    '''
+    Correct the spelling of a word using the spell checker.
+    :param word: The word to correct.
+    :return: The corrected word.
+    '''
+    corrected_word = spell.correction(word)
+    return corrected_word if corrected_word else word
+
+
+def preprocess_text(text: str, spell_check: bool =True) -> str:
+    '''
+    Preprocess the input text by normalizing it to lower case and removing special characters.
+    :param text: The input text to preprocess.
+    :return: The preprocessed text.
+    '''
+    # Normalize input text to lower case and remove special characters
+    cleaned_text = re.sub(r"[^a-z0-9\s]", "", text)
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text).strip()
+    
+    tokens = cleaned_text.split()
+    
+    # Loop through tokens and correct spelling
+    if spell_check:
+        tokens = [correct_spelling(token) for token in tokens]
+    
+    text = " ".join(tokens).upper()
+    
+    return text
+
+
+def add_leading_zero(match: re.Match) -> str:
+    '''
+    Add leading zero to the hour in the time string.
+    :param match: The regex match object.
+    :return: The time string with leading zero.
+    '''
+    return f'{int(match.group(1)):02}:00{match.group(2)}'
+
+
+def format_time(time_str: str) -> str:
+    '''
+    Format the time string to a standard format.
+    :param time_str: The input time string.
+    :return: The formatted time string.
+    '''
+    
+    # Regex pattern to find times like '1am', '1pm', etc and convert them to '01:00am', '01:00pm'
+    pattern = r'(?<=\b)(\d{1})(am|pm)\b'
+    time_str = re.sub(pattern, add_leading_zero, time_str)
+    
+    # Regex to remove any space between the time and the 'AM/PM'
+    time_str = re.sub(r'(\d{1,2}:\d{2})(am|pm)', r'\1 \2', time_str, flags=re.IGNORECASE)
+
+    return time_str
+
+
+def preprocess_time(text: str) -> str:
+    '''
+    Preproces the input text for time, remove extra spaces and correct am/pm format.
+    :param text: The input text to preprocess.
+    :return: The preprocessed text.
+    '''
+    cleaned_text = text.capitalize()
+    
+    cleaned_text = format_time(cleaned_text)
+    
+    return cleaned_text
+
+
+def parse_time(journey: dict[str, str]) -> str:
+    '''
+    Parse the time from the journey dictionary.
+    :param journey: The dictionary containing the time information.
+    :return: The parsed time in the format 'YYYY-MM-DD HH:MM:SS'.
+    '''
+    parsed_date = datetime.now()
+    
+    for key in journey.keys():
+        for value in journey[key]:
+            settings = {"PREFER_DATES_FROM": "future", "RELATIVE_BASE": parsed_date, "TIMEZONE": "GMT", "DATE_ORDER": "DMY"}
+            result = dateparser.parse(value, settings=settings)
+            if not result:
+                continue
+            parsed_date = result
+    return parsed_date.strftime("%Y-%m-%d %H:%M:%S")
